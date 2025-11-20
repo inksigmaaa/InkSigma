e0158be40100e4cbaf835aded170ba325fe57c35
Logesh-sen <logesh.r@senatio.com>
2025-11-20 16:52:33 +0530

    completely changed the schedule page

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,54 +1,35 @@
 "use client"
 
-import { useState } from "react";
 import NavbarLoggedin from "../components/navbar/NavbarLoggedin";
 import Sidebar from "../components/sidebar/Sidebar";
 import Verify from "../components/verify/Verify";
-import PersonalArticles from "../components/personalArticles/personalArticles";
+import SchedulePageClient from "../components/schedule/SchedulePageClient";
 
 export default function SchedulePage() {
-  const [selectedArticles, setSelectedArticles] = useState([]);
-
-  const articles = [
+  // Mock data matching the image design
+  const posts = [
     {
       id: 1,
-      status: "scheduled",
       title: "Title of the Blog will be in this area",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum efficitur tortorsdkhbishdoisa...",
-      categories: ["Sports", "Humour", "History"],
-      postedTime: "Posted 2 mins ago",
+      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum efficitur tortorsdkhbishdoisa...",
+      tags: ["Sports", "Humour", "History"],
+      postedTime: "Posted on 2nd Feb, 2023",
     },
     {
       id: 2,
-      status: "scheduled",
-      title: "Another Scheduled Blog Title",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum efficitur tortorsdkhbishdoisa...",
-      categories: ["Technology", "Business"],
-      postedTime: "Posted 5 mins ago",
+      title: "Title of the Blog will be in this area",
+      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum efficitur tortorsdkhbishdoisa...",
+      tags: ["Sports", "Humour", "History"],
+      postedTime: "Posted on 2nd Feb, 2023",
     }
   ];
-
-  const handleArticleSelect = (id, isSelected) => {
-    setSelectedArticles(prev => 
-      isSelected 
-        ? [...prev, id]
-        : prev.filter(articleId => articleId !== id)
-    );
-  };
 
   return (
     <>
       <NavbarLoggedin />
       <Sidebar />
       <Verify />
-      <PersonalArticles
-        title="Schedule"
-        titleColor="#0EA5E9"
-        articles={articles}
-        emptyMessage="No scheduled articles yet"
-        selectedArticles={selectedArticles}
-        onArticleSelect={handleArticleSelect}
-      />
+      <SchedulePageClient posts={posts} />
     </>
   )
 }
