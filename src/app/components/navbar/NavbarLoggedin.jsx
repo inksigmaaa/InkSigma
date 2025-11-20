71b78ebdc3632f29be62352586398230460e666b
Gugan <gugan@Tennyson-MacBook-Air.local>
2025-11-20 17:29:36 +0530

    Update components and pages

--- src/app/components/navbar/NavbarLoggedin.jsx
+++ src/app/components/navbar/NavbarLoggedin.jsx
@@ -177,13 +177,13 @@ export default function NavbarLoggedin() {
                                 className="absolute top-[50px] right-0 w-[200px] bg-white shadow-[0_4px_24px_rgb(0,0,0,0.1)] border border-[#EDEDED] rounded-lg flex flex-col gap-1 p-2 z-[99999] max-md:fixed max-md:top-[80px] max-md:right-5 max-md:w-[180px] max-md:shadow-[0_8px_32px_rgba(0,0,0,0.2)]"
                             >
                                 <a href="/profile-settings"
-                                    className="px-2 py-1 rounded text-[12px] font-normal text-[#b0b0b0] hover:text-black">
+                                    className="px-2 py-1 rounded text-[12px] font-normal text-gray-700 hover:text-black">
                                     Profile Settings
                                 </a>
 
                                 <button
                                     onClick={handleLogout}
-                                    className="px-2 py-1 rounded text-[12px] font-normal text-[#b0b0b0] hover:text-black text-left">
+                                    className="px-2 py-1 rounded text-[12px] font-normal text-text-gray-700 hover:text-black text-left">
                                     Logout
                                 </button>
                             </div>
