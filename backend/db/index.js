51e3bcbc807e8c6fdc3eab341fa5dc891be134a8
Logesh-sen <logesh.r@senatio.com>
2025-11-24 12:12:56 +0530

    pull from my last commit

--- backend/db/index.js
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema.js";
-
-const connectionString = process.env.DATABASE_URL || "";
-
-// Only create client if DATABASE_URL is available (runtime)
-const client = connectionString ? postgres(connectionString) : null;
-export const db = client ? drizzle(client, { schema }) : null;
