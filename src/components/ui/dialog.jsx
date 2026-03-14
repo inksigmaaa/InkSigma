f663516b31c261a860c570576c048ace3c69715b
test-gugan <thalapathigugan.k@senatio.com>
2026-03-14 15:50:59 +0530

    style: Remove specific focus ring offset and color classes from DialogClose button.

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -23,7 +23,7 @@ const DialogContent = React.forwardRef(
       {showClose && (
         <DialogClose
           asChild
-          className="absolute right-4 top-4 mt-0 h-auto w-auto border-0 bg-transparent p-0 opacity-70 shadow-none ring-offset-white hover:bg-transparent hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 data-[state=open]:bg-transparent"
+          className="absolute right-4 top-4 mt-0 h-auto w-auto border-0 bg-transparent p-0 opacity-70 shadow-none  hover:bg-transparent hover:opacity-100 focus:outline-none data-[state=open]:bg-transparent"
         >
           <button type="button" aria-label="Close dialog">
             <X className="h-5 w-5" />
