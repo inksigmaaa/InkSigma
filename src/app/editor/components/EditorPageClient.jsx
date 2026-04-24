75a5c1403eae2b8961ad350524735fab9fc04028
test-gugan <thalapathigugan.k@senatio.com>
2026-04-24 15:55:41 +0530

    Keep draft recovery but remove recovery toast

--- src/app/editor/components/EditorPageClient.jsx
+++ src/app/editor/components/EditorPageClient.jsx
@@ -32,6 +32,10 @@ import {
   writeLocalArticleId,
 } from "./services/DraftRecoveryService";
 import { getApiBase } from "@/utils/apiBase";
+import {
+  getDraft as dexieGetDraft,
+  deleteDraft as dexieDeleteDraft,
+} from "./services/DexieService";
 import SaveStatusIndicator from "./SaveStatusIndicator";
 import EditorPageStyles from "./EditorPageStyles";
 import EditorStatsBar from "./EditorStatsBar";
@@ -777,7 +781,7 @@ export default function EditorPageClient() {
       initialBlogIdRef.current = blogId;
       lastLoadedBlogIdRef.current = blogId;
     }
-  }, [blogId, isMounted, getDexieId, loadExistingBlog]);
+  }, [blogId, isMounted, getDexieId, loadExistingBlog, restoreLocalDraft]);
 
   useEffect(() => {
     if (
