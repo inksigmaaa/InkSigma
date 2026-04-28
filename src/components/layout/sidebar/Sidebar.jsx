c4f850f6bdcb7949f68613798a8f721e902f4160
test-gugan <thalapathigugan.k@senatio.com>
2026-04-28 16:29:31 +0530

    Hide sidebar view site on mobile

--- src/components/layout/sidebar/Sidebar.jsx
+++ src/components/layout/sidebar/Sidebar.jsx
@@ -308,7 +308,7 @@ function Sidebar() {
         {/* SIDEBAR CONTAINER */}
         <div className="relative w-[165px] h-[612px] bg-white border-r border-gray-200 p-[14px] pr-[10px] flex flex-col gap-[10px] overflow-hidden pointer-events-auto max-md:w-auto max-md:min-w-max max-md:h-[70px] max-md:px-4 max-md:py-2 max-md:flex-row max-md:gap-2 max-md:border-r-0 max-md:overflow-visible ">
           {/* PROFILE */}
-          <div className="flex items-center gap-2 pb-[10px] max-md:hidden">
+          <div className="hidden md:flex items-center gap-2 pb-[10px]">
             <Avatar className="w-[34px] h-[34px] border-2 border-violet-500 flex-shrink-0 bg-gray-100">
               {loading ? (
                 <AvatarFallback className="w-full h-full bg-gray-100 flex items-center justify-center">
